6ba7e38a0133624bf30737e22f32aa60a7eee84a
jwooo <parkjwoo0515@gmail.com>
2023-05-21 21:08:02 +0900

    여덟 번째 테스트: 대문자 포함 조건만 충족하는 경우

--- src/main/java/com/tdd/chap02/PasswordStrengthMeter.java
+++ src/main/java/com/tdd/chap02/PasswordStrengthMeter.java
@@ -11,6 +11,7 @@ public class PasswordStrengthMeter {
 
         if (lengthEnough && !containsNum && !containsUpp) return PasswordStrength.WEAK;
         if (!lengthEnough && containsNum && !containsUpp) return PasswordStrength.WEAK;
+        if (!lengthEnough && !containsNum && containsUpp) return PasswordStrength.WEAK;
 
         if (!lengthEnough) return PasswordStrength.NORMAL;
         if (!containsNum) return PasswordStrength.NORMAL;
