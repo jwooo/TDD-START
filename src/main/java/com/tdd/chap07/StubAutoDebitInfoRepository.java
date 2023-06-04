4248beb68550fbc76004c45edd7d28a51008bd32
jwooo <parkjwoo0515@gmail.com>
2023-06-04 14:12:39 +0900

    StubAutoDebitInfoRepository 구현

---
+++ src/main/java/com/tdd/chap07/StubAutoDebitInfoRepository.java
@@ -0,0 +1,13 @@
+package com.tdd.chap07;
+
+public class StubAutoDebitInfoRepository implements AutoDebitInfoRepository{
+    @Override
+    public void save(AutoDebitInfo info) {
+
+    }
+
+    @Override
+    public AutoDebitInfo findOne(String userId) {
+        return null;
+    }
+}
