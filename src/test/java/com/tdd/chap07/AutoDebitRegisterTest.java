920db1bf58a0822e959742f22f26dd9f316cd688
jwooo <parkjwoo0515@gmail.com>
2023-06-05 22:24:10 +0900

    대역을 통한 테스트 코드 수정

--- src/test/java/com/tdd/chap07/AutoDebitRegisterTest.java
+++ src/test/java/com/tdd/chap07/AutoDebitRegisterTest.java
@@ -10,25 +10,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AutoDebitRegisterTest {
 
     private AutoDebitRegister register;
+    private StubCardNumberValidator stubValidator;
+    private StubAutoDebitInfoRepository stubRepository;
 
     @BeforeEach
     void setUp() {
-        CardNumberValidator validator = new CardNumberValidator();
-        AutoDebitInfoRepository repository = new JpaAutoDebitInfoRepository();
-        register = new AutoDebitRegister(validator, repository);
+        stubValidator = new StubCardNumberValidator();
+        stubRepository = new StubAutoDebitInfoRepository();
+        register = new AutoDebitRegister(stubValidator, stubRepository);
     }
 
     @Test
     void validCard() {
+        stubValidator.setInvalidNo("111122223333");
+
         AutoDebitReq req = new AutoDebitReq("user1", "1234123412341234");
-        RegisterResult result = this.register.register(req);
+        RegisterResult result = register.register(req);
         assertEquals(VALID, result.getValidity());
     }
 
     @Test
     void theftCard() {
+        stubValidator.setTheftNo("1234567890123456");
+
         AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
-        RegisterResult result = this.register.register(req);
+        RegisterResult result = register.register(req);
         assertEquals(THEFT, result.getValidity());
     }
 }
