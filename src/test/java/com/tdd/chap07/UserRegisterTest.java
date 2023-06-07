3cba181f6ac19db7f70b3aa89309c084491fe5cd
jwooo <parkjwoo0515@gmail.com>
2023-06-07 20:04:41 +0900

    최초 UserRegisterTest 코드

---
+++ src/test/java/com/tdd/chap07/UserRegisterTest.java
@@ -0,0 +1,30 @@
+package com.tdd.chap07;
+
+import com.tdd.chap07.user.StubWeakPasswordChecker;
+import com.tdd.chap07.user.UserRegister;
+import com.tdd.chap07.user.WeakPasswordException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserRegisterTest {
+    private UserRegister userRegister;
+    private StubWeakPasswordChecker stubPasswordChecker;
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(stubPasswordChecker);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        stubPasswordChecker.setWeak(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+}
