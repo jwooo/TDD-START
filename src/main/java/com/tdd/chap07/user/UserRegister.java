bf0334722524de0f3884a93bc926fa2ad5e24fcb
jwooo <parkjwoo0515@gmail.com>
2023-06-10 15:26:13 +0900

    의존 대상에 필요한 기능 도출

--- src/main/java/com/tdd/chap07/user/UserRegister.java
+++ src/main/java/com/tdd/chap07/user/UserRegister.java
@@ -8,6 +8,8 @@ public class UserRegister {
     }
 
     public void register(String id, String pw, String email) {
-        throw new WeakPasswordException();
+        if (passwordChecker.checkPasswordWeak(pw)) {
+            throw new WeakPasswordException();
+        }
     }
 }
