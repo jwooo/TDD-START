a5e24ba38545a8ae690c483f5517a7cf3add637f
jwooo <parkjwoo0515@gmail.com>
2023-06-13 20:20:45 +0900

    MemoryUserRepository 클래스에 save() 구현

--- src/main/java/com/tdd/chap07/user/MemoryUserRepository.java
+++ src/main/java/com/tdd/chap07/user/MemoryUserRepository.java
@@ -1,4 +1,12 @@
 package com.tdd.chap07.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryUserRepository implements UserRepository {
+    private Map<String, User> users = new HashMap<>();
+    @Override
+    public void save(User user) {
+        users.put(user.getId(), user);
+    }
 }
