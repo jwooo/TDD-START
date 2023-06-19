ddb1726ccaec9ed20dbf8e7f8698aec14ab31e2f
jwooo <parkjwoo0515@gmail.com>
2023-06-19 19:34:46 +0900

    MemoryUserRepository 클래스에 findById() 메서드 구현

--- src/main/java/com/tdd/chap07/user/MemoryUserRepository.java
+++ src/main/java/com/tdd/chap07/user/MemoryUserRepository.java
@@ -9,4 +9,9 @@ public class MemoryUserRepository implements UserRepository {
     public void save(User user) {
         users.put(user.getId(), user);
     }
+
+    @Override
+    public User findById(String id) {
+        return users.get(id);
+    }
 }
