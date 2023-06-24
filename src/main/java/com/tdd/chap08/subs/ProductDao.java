4a6bfd594c448fd0674f890d27fe6a29c43bc494
jwooo <parkjwoo0515@gmail.com>
2023-06-24 20:49:06 +0900

    실행 시점에 따라 달라지는 결과

---
+++ src/main/java/com/tdd/chap08/subs/ProductDao.java
@@ -0,0 +1,5 @@
+package com.tdd.chap08.subs;
+
+public interface ProductDao {
+    Product selectById(String productId);
+}
---
+++ src/main/java/com/tdd/chap08/subs/SubscriptionDao.java
@@ -0,0 +1,7 @@
+package com.tdd.chap08.subs;
+
+public interface SubscriptionDao {
+    Subscription selectByUser(String id);
+
+    void insert(Subscription subscription);
+}
