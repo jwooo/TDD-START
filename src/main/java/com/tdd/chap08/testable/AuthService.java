349c9f8e18aa77bf44cd57d8a90dcef9d0ebf1b1
jwooo <parkjwoo0515@gmail.com>
2023-06-25 17:45:21 +0900

    외부 라이브러리를 감싼 클래스

---
+++ src/main/java/com/tdd/chap08/testable/AuthService.java
@@ -0,0 +1,16 @@
+package com.tdd.chap08.testable;
+
+import com.tdd.chap08.auth.AuthUtil;
+
+public class AuthService {
+    private String authKey = "someKey";
+
+    public int authenticate(String id, String pw) {
+        boolean authorized = AuthUtil.authorize(authKey);
+        if (authorized) {
+            return AuthUtil.authenticate(id, pw);
+        } else {
+            return -1;
+        }
+    }
+}
